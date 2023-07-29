9568899bf8bca17ab5fd7c2fca9eaaab621202f2
learntiffy <learn.tiffy@gmail.com>
2023-07-29 14:19:37 +0530

    sorted by type

--- src/app/checkout/checkout.page.ts
+++ src/app/checkout/checkout.page.ts
@@ -6,6 +6,7 @@ import { Item } from '../models/Item';
 import { Address } from '../models/request/Address';
 import * as CryptoJS from 'crypto-js';
 import { environment } from 'src/environments/environment';
+import { ItemType } from '../models/ItemType';
 
 @Component({
   selector: 'app-checkout',
@@ -18,6 +19,13 @@ export class CheckoutPage implements OnInit, OnChanges {
   mealType = '';
   selectedAddress!: Address;
   items: Item[] = [];
+  itemOrderMap = new Map<string, number>([
+    [ItemType.SABJI, 1],
+    [ItemType.ROTI, 2],
+    [ItemType.DAL, 3],
+    [ItemType.RICE, 4],
+    [ItemType.SPECIAL, 5],
+  ]);
 
   constructor(
     private userService: UserService,
@@ -27,16 +35,17 @@ export class CheckoutPage implements OnInit, OnChanges {
   ngOnInit() {
     this.mealDay = this.route.snapshot.paramMap.get('day') ?? '';
     this.mealType = this.route.snapshot.paramMap.get('meal') ?? '';
-    setTimeout(() => {this.isLoading = false}, 2000)
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
     this.userService.checkoutMap.forEach((val, key) => {
       this.items.push(...val);
-    })
+    });
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-      console.log(changes)
+    console.log(changes);
   }
-
 
   prepareOrder() {
     var today = new Date();
@@ -44,7 +53,6 @@ export class CheckoutPage implements OnInit, OnChanges {
     tomorrow.setDate(today.getDate() + 1);
 
     var encryptedOrder;
-    console.log(typeof this.selectedAddress)
     if (this.mealDay !== '' && this.mealType !== '') {
       const order = {
         address: this.selectedAddress._id,
@@ -52,7 +60,7 @@ export class CheckoutPage implements OnInit, OnChanges {
         mealDate: this.mealDay === 'TODAY' ? today : tomorrow,
         items: this.setOrderItems(),
       };
-      console.log(order)
+      console.log({order})
       encryptedOrder = CryptoJS.AES.encrypt(
         JSON.stringify(order),
         environment.AES_SECRET
@@ -67,13 +75,28 @@ export class CheckoutPage implements OnInit, OnChanges {
 
   setOrderItems(): string[] {
     const items: string[] = [];
+    var itemsArray: Item[] = [];
     this.userService.checkoutMap.forEach((val, key) => {
-      val.forEach(v => items.push(v._id));
+      val.forEach((v) => {
+        // items.push(v._id);
+        itemsArray.push(v);
+      });
     });
+    this.sortOrderByType(itemsArray);
+    console.log(itemsArray)
+    itemsArray.forEach(v => items.push(v._id));
     return items;
   }
 
   ionViewWillEnter() {
     this.userService.setHeaderTitle(Page.CHECKOUT);
+  }
+
+  sortOrderByType(items: Item[]) {
+    items.sort(
+      (a, b) =>
+        (this.itemOrderMap.get(a.type) ?? 0) -
+        (this.itemOrderMap.get(b.type) ?? 0)
+    );
   }
 }
