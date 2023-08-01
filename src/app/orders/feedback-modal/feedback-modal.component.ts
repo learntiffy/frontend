8d85e50af850f17f6658ad886b8b9e2201a180ec
learntiffy <139127232+tiffylearn@users.noreply.github.com>
2023-08-01 22:51:05 +0530

    Feeback modal issue fixed

--- src/app/orders/feedback-modal/feedback-modal.component.ts
+++ src/app/orders/feedback-modal/feedback-modal.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { IonModal } from '@ionic/angular';
 
 @Component({
@@ -6,29 +13,31 @@ import { IonModal } from '@ionic/angular';
   templateUrl: './feedback-modal.component.html',
   styleUrls: ['./feedback-modal.component.scss'],
 })
-export class FeedbackModalComponent  implements OnInit {
-  @ViewChild(IonModal) modal!: IonModal;
+export class FeedbackModalComponent implements OnInit {
+  @ViewChild('modal', { static: false }) modal!: IonModal;
+  @Output() close = new EventEmitter<void>();
+  @Input() isModalOpen!: boolean;
   rating = 5;
-  message : string = "Rate the food:";
+  message: string = 'Rate the food:';
 
   comment!: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
 
   cancel() {
     this.modal.dismiss(null, 'cancel');
+    this.close.emit();
   }
 
   confirm() {
     this.modal.dismiss(this.comment, 'confirm');
+    this.close.emit();
   }
 
   onWillDismiss(event: Event) {
-    // const ev = event as CustomEvent<OverlayEventDetail<string>>;
-    // if (ev.detail.role === 'confirm') {
-    //   this.message = `Hello, ${ev.detail.data}!`;
-    // }
+    this.modal.dismiss(null, 'cancel');
+    this.close.emit();
   }
 }
--- src/app/orders/order-card/order-card.component.ts
+++ src/app/orders/order-card/order-card.component.ts
@@ -6,30 +6,27 @@ import { Order } from 'src/app/models/Order';
   templateUrl: './order-card.component.html',
   styleUrls: ['./order-card.component.scss'],
 })
-export class OrderCardComponent  implements OnInit {
-
+export class OrderCardComponent implements OnInit {
   @Input() order!: Order;
   allItemsName = '';
   total = 0;
 
   showFeedbackModal = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.order.items.forEach((item, i) => {
-      console.log(item, i);
       this.total += item.price;
-      if(i < this.order.items.length-1){
-        this.allItemsName += (item.name + ', ');
+      if (i < this.order.items.length - 1) {
+        this.allItemsName += item.name + ', ';
       } else {
         this.allItemsName += item.name;
       }
-    })
+    });
   }
 
   openFeedbackModel() {
     this.showFeedbackModal = true;
   }
-
 }
