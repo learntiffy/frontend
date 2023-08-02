3786aa1ad5724e2f77702492b6b180b779312484
learntiffy <139127232+tiffylearn@users.noreply.github.com>
2023-08-02 22:29:44 +0530

    Login issue resolved

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -56,7 +56,8 @@ export class LoginPage {
           next: (response) => {
             if (response.status === 200) {
               localStorage.setItem('token', response.data);
-              this.authService.setIsLoggedIn();
+              // this.authService.setIsLoggedIn();
+              this.authService.loginStatus.emit(true);
               this.router.navigateByUrl('/');
             } else {
               this.userService.presentToast(response.message);
